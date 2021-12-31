3dae2477832bca697daa8dcdba2c72a71147ce2d
junior <mjuniorramos112@gmail.com>
2021-12-30 21:11:43 -0300

    Validação via token

--- login/authentication/views.py
+++ login/authentication/views.py
@@ -1,3 +1,4 @@
+from django.core.mail.message import EmailMessage
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -5,7 +6,11 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from login import settings
 from django.core.mail import send_mail
-
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from token import generate_token
 
 def home(request):
     return render(request, "authentication/index.html")
@@ -43,16 +48,37 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-
+        myuser.is_active = False
         myuser.save()
 
         messages.success(request, "Sua conta foi criada com sucesso. Nós te enviamos um email de confirmação de conta!")
+
+        #Bem vindo no Email
 
         subject = "Bem vindo a um sistema simples de login em Django"
         message = "Hello" + myuser.first_name + "!! /n/n" + "Bem vindo a um sistema simples de login em Django, por gentileza, confirme seu endereço de email \n" + "Obrigado, atenciosamente, \n Junior."
         from_email = settings.EMAIL_HOST_USER
         to_list = [myuser.email]
         send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+        #Confirmacao de email
+
+        current_site = get_current_site(request)
+        email_subject = "Confirme seu email @ Login System - Django"
+        message2 = render_to_string("email_confirmation.html", {dict}), {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),       
+            'token': generate_token.make_token(myuser)
+        }
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email], 
+        )
+        email.fail_silently = True
+        email.send()
 
         return redirect('signin')
 
@@ -83,3 +109,17 @@ def signout(request):
     messages.success(request,"Você se desconectou com sucesso!")
     return redirect('home')
 
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request,'activate_failed.html')
